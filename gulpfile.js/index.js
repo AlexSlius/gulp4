c2de2964018b2f028e71e1c5a3ad5fa181cbe1f5
AlexSlius <alex.sliusarchyk@gmail.com>
2023-09-27 16:12:32 +0300

    renewal

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -6,6 +6,10 @@ const concat = require('gulp-concat');
 const ttf2woff2 = require('gulp-ttf2woff2');
 const ttf2woff = require('gulp-ttf2woff');
 const webp = require('gulp-webp');
+const fileinclude = require('gulp-file-include');
+const webpHTML = require('gulp-webp-html');
+const clean = require('gulp-clean');
+const babel = require('gulp-babel');
 
 // ways
 const sourse_folder = '#app/';
@@ -13,15 +17,21 @@ const project_folder = 'dist/';
 
 const paths = {
 	entry: {
+		html: `${sourse_folder}**/*.html`,
+		htmlNoComponent: `!${sourse_folder}**/_*.html`,
+		script: `${sourse_folder}scripts/**/*.js`,
 		sass: `${sourse_folder}styles/main.scss`,
 		fonts: `${sourse_folder}fonts/**/*.ttf`,
 		img: `${sourse_folder}img/**/*.{jpg, png, svg, ico, webp}`
 	},
 	out: {
+		html: `${project_folder}`,
+		script: `${project_folder}scripts`,
 		css: `${project_folder}styles`,
 		fonts: `${project_folder}fonts`,
 		img: `${project_folder}img`
-	}
+	},
+	clean: `./${project_folder}`
 };
 
 // live server
@@ -45,13 +55,35 @@ function TSass() {
 		.pipe(MbrowserSync.stream())
 }
 
-function TJavaScripts() {
-	
+// html
+function THtml() {
+	return src([paths.entry.html, paths.entry.htmlNoComponent])
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file'
+		}))
+		.pipe(webpHTML())
+		.pipe(dest(paths.out.html))
+		.pipe(MbrowserSync.stream())
+}
+
+// scripts
+function TScripts() {
+	return src([paths.entry.script])
+		.pipe(fileinclude())
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(concat('main.js'))
+		.pipe(dest(paths.out.script))
+		.pipe(MbrowserSync.stream())
 }
 
 // watch all
 function TstartWatch() {
+	watch([paths.entry.html], THtml);
 	watch([paths.entry.sass], TSass);
+	watch([paths.entry.script], TScripts);
 }
 
 // fonts woff
@@ -75,7 +107,14 @@ function TImageConverToWebp() {
 		.pipe(dest(paths.out.img))
 }
 
+function Tclean() {
+	return src(paths.clean, { read: false })
+		.pipe(clean(paths.clean))
+}
+
 exports.live = Tbrowsersync;
 exports.images = TImageConverToWebp;
+exports.clean = Tclean;
 exports.fonts = series(TFontfConverTowoff, TFontfConverTowoff2);
-exports.dev = parallel(TSass, Tbrowsersync, TstartWatch);
+exports.build = series(THtml, TScripts, TSass, TFontfConverTowoff, TFontfConverTowoff2);
+exports.dev = series(THtml, TScripts, TSass, TFontfConverTowoff, TFontfConverTowoff2, parallel(Tbrowsersync, TstartWatch));
